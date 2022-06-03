dcdc7b25237244c5585c9a25ce6786b14f86d59e
SpherePerfect <72667146+SpherePerfect@users.noreply.github.com>
2022-06-03 12:44:39 +0530

    Add files via upload

---
+++ astronauts_stars.py
@@ -0,0 +1,19 @@
+print("Enter the number of stars you want - ")
+entered_number = int(input())
+print("Enter True or False - ")
+entered_binary = str(input())
+
+if entered_binary == "True":
+    i = 0
+    while i < entered_number:
+        print("*"*i)
+        i = i+1
+elif entered_binary == "False":
+    i = entered_number
+    while i != 1:
+        print(i)
+        print("*" * i)
+        i = i-1
+
+
+
